05c9491747c84671c3d3af256804393f7984cf6b
riko0000 <mariko.s0807@gmail.com>
2018-12-23 10:01:46 +0900

    Updates

--- precache-manifest.1e2c1a9231aee1bf1ab008414bd776e6.js
+++ precache-manifest.1e2c1a9231aee1bf1ab008414bd776e6.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/bell_pj/static/js/runtime~main.f69d6bb0.js"
   },
   {
-    "revision": "f42d2065edcf1e3246b3",
-    "url": "/bell_pj/static/js/main.f42d2065.chunk.js"
+    "revision": "24c40009bda638b61d84",
+    "url": "/bell_pj/static/js/main.24c40009.chunk.js"
   },
   {
     "revision": "d9368836f5fd68cb464e",
     "url": "/bell_pj/static/js/1.d9368836.chunk.js"
   },
   {
-    "revision": "f42d2065edcf1e3246b3",
-    "url": "/bell_pj/static/css/main.a23ed5bb.chunk.css"
+    "revision": "24c40009bda638b61d84",
+    "url": "/bell_pj/static/css/main.eb664638.chunk.css"
   },
   {
-    "revision": "059041b6e1e2b625ab4259f868d6aec5",
+    "revision": "8d1f483a7db45641c7601d4f75816cfd",
     "url": "/bell_pj/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/bell_pj/precache-manifest.7f6cfb6221020a50ca7b9c045a60f792.js"
+  "/bell_pj/precache-manifest.1e2c1a9231aee1bf1ab008414bd776e6.js"
 );
 
 workbox.clientsClaim();
